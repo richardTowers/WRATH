b3c1a021fd80f470cd3d873840491e933295f798
Richard Towers <richard_towers@Richard-Towers-Macbook.local>
2013-07-27 19:22:04 +0100

    Switching to mustache.

--- js/wrath.js
+++ js/wrath.js
@@ -5,29 +5,25 @@ require.config({
 		'angular-resource': 'angular-resource-unstable/angular-resource',
 		'codemirror': 'codemirror/lib/codemirror',
 		'codemirror-xml': 'codemirror/mode/xml/xml',
-		'codemirror-clike': 'codemirror/mode/clike/clike',
-		'ui-codemirror': 'angular-ui-codemirror/ui-codemirror'
+		'codemirror-javascript': 'codemirror/mode/javascript/javascript',
+		'ui-codemirror': 'angular-ui-codemirror/ui-codemirror',
+		'mustache':'mustache/mustache'
 	},
 	shim: {
 		'angular': { exports: 'angular'	},
 		'angular-resource': { exports: 'ngResource', deps: ['angular'] },
 		'codemirror': { exports: 'CodeMirror' },
 		'codemirror-xml': { deps: ['codemirror'] },
-		'codemirror-clike': { deps: ['codemirror'] },
-		'ui-codemirror': { deps: ['angular','codemirror','codemirror-xml','codemirror-clike'] }
+		'codemirror-javascript': { deps: ['codemirror'] },
+		'ui-codemirror': { deps: ['angular','codemirror','codemirror-xml','codemirror-javascript'] }
 	}
 });
-require(['angular', 'angular-resource', 'ui-codemirror'], function(angular, uiCodemirror) {
+require(['angular', 'angular-resource', 'ui-codemirror', 'mustache'], function(angular, angularResource, uiCodemirror, mustache) {
 
 	angular.module('wrathControllers', ['ngResource'])
 		.controller('runnerController', ['$scope', '$http', '$resource', function($scope, $http , $resource) {
 
 			$http.defaults.useXDomain = true;
-			var transform = $resource(
-				'http://richardtowers.apiary.io/WSPipeline/Transform',
-				{},
-				{run: {method:'PUT'}});
-
 			var run = $resource('http://richardtowers.apiary.io/WSPipeline/Run');
 
 			$scope.runRequest = function () {
@@ -44,35 +40,42 @@ require(['angular', 'angular-resource', 'ui-codemirror'], function(angular, uiCo
 			};
 
 			$scope.runTransform = function () {
-				transform.run({
-					'previousResponses' : [],
-					'prelude' : '// Some C#',
-					'template' : '<InitAddContact />',
-					'environment' : 'http://devbintproc3/www/service.svc',
-					'user' : 'AHTEST01',
-					'password' : 'GSAHJDGJAHS'
-				},
-				function (x) {
-					$scope.request.value = x.response;
-					$scope.request.show = true;
-				});
+				var preludeFunction, model, result;
+				try
+				{
+					// Create a function from the prelude
+					preludeFunction = new Function($scope.prelude.value);
+					// Run the prelude
+					model = preludeFunction();
+					// Transform the template
+					result = mustache.render($scope.template.value, model);
+				}
+				catch(exception)
+				{
+					result = exception.message;
+				}
+				// Set the request to be the result of the template
+				$scope.request.value = result;
+				$scope.request.show = true;
 			};
 
-			$scope.prelude = { show: true, value: '// Some C#' };
-			$scope.template = { show: true, value: '<InitAddContact />' };
+			$scope.prelude = {
+				show: true,
+				value: '// JavaScript\nreturn {\n  key: "value"\n};' };
+			$scope.template = { show: true, value: '<SomeXml>\n  <Element Value="{{key}}" />\n</SomeXml>' };
 			$scope.request = { show: true, value: '' };
 			$scope.response = { show: true, value: '' };
 
-			$scope.getWidthClass = function () {
+			$scope.getWidthClass = function (visible) {
 				var count = [
 						$scope.prelude.show,
 						$scope.template.show,
 						$scope.request.show,
 						$scope.response.show].filter(function(x){return x;}).length;
-				return count === 0 ? 'span1' : 'span' + (12 / count);
+				return count === 0 ? 'zeroWidth' : 'span' + (12 / count);
 			};
 
-			$scope.cSharpMirrorOptions = { mode: 'text/x-csharp', theme: 'monokai', lineNumbers: true };
+			$scope.jsMirrorOptions = { mode: 'javascript', theme: 'monokai', lineNumbers: true };
 			$scope.xmlMirrorOptions = { mode: 'xml', theme: 'monokai', lineNumbers: true };
 		}]);
 
